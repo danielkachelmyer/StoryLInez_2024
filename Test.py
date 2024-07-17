87cd3f43b0bfce3fb08680448e31a9c0eed5e5ff
daniel kachelmyer <danielkachelmyer@gmail.com>
2024-07-17 14:31:02 -0500

    beautifulSoup4

--- Test.py
+++ Test.py
@@ -1,2 +1,12 @@
-print("Hello world")
-print("Hello world")
+from bs4 import BeautifulSoup
+
+with open("Index.html", "r") as f:
+    doc = BeautifulSoup(f, "html.parser")
+
+tag = doc.title
+print(tag.string)
+#print(doc)
+
+a_tags = doc.find_all("p")[0]
+
+print(a_tags.find_all("b"))
